051a1e8c7807a074d3de340091300da002c1e438
Martin-Minev <72812000+Martin-Minev@users.noreply.github.com>
2021-02-24 00:04:30 +0100

    1st

---
+++ Race.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheRace
+{
+    public class Race
+    {
+        private List<Racer> data;
+
+        public Race(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            data = new List<Racer>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get { return data.Count; } }
+
+        public void Add(Racer racer)
+        {
+            if (data.Count < this.Capacity)
+            {
+                data.Add(racer);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Racer racer = data.FirstOrDefault(r => r.Name == name);
+            if (racer == null)
+            {
+                return false;
+            }
+            data.Remove(racer);
+            return true;
+        }
+
+        public Racer GetOldestRacer()
+        {
+            return data.OrderByDescending(r => r.Age).FirstOrDefault();
+        }
+
+        public Racer GetRacer(string name)
+        {
+            Racer racer = data.FirstOrDefault(r => r.Name == name);
+            return racer;
+        }
+
+        public Racer GetFastestRacer()
+        {
+            return data.OrderByDescending(r => r.Car.Speed).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"Racers participating at {Name}: ");
+            foreach (Racer racer in data)
+            {
+                result.AppendLine($"{racer.Name}, {racer.Age} ({racer.Country})");
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ StartUp.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace TheRace
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Race race = new Race("Indianapolis 500", 10);
+
+            //Initialize cars 
+
+            Car car1 = new Car("Ferrari", 350);
+            Car car2 = new Car("lambo", 170);
+            Car car3 = new Car("Lada", 1700);
+
+            //Initialize racer1 
+
+            Racer racer1 = new Racer("Stephen", 40, "Bulgaria", car1);
+            Racer racer3 = new Racer("Mao", 400, "China", car3);
+            race.Add(racer3);
+            Console.WriteLine(racer3);
+
+            //Print Racer 
+
+            Console.WriteLine(racer1); //Racer: Stephen, 40 (Bulgaria) 
+
+            //Add Racer 
+
+            race.Add(racer1);
+
+            //Remove Racer 
+
+            race.Remove("Vin Benzin"); //false 
+
+            Racer racer2 = new Racer("Mark", 34, "UK", car2);
+
+            //Add Racer 
+
+            race.Add(racer2);
+
+            Racer oldestRacer = race.GetOldestRacer(); // Racer with name Stephen 
+
+            Racer racerStephen = race.GetRacer("Stephen"); // Racer with name Stephen 
+
+            ///Racer fastestRacer = race.GetFastestRacer(); // Racer with name Mark 
+
+            Console.WriteLine(oldestRacer); //Racer: Stephen, 40 (Bulgaria) 
+
+            Console.WriteLine(racerStephen); //Racer: Stephen, 40 (Bulgaria) 
+
+           /// Console.WriteLine(fastestRacer); // Racer: Mark, 34 (UK) 
+
+            Console.WriteLine(race.Count); //2 
+
+            Console.WriteLine(race.Report());
+        }
+    }
+}
